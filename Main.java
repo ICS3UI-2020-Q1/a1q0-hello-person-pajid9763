2c4be6637a7193a87599969045cce0fc210be7a4
pajid9763 <pajid9763@wrdsb.ca>
2020-09-11 21:53:54 +0000

    Finished Hello Person program

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * a program to say hello to a user and calculate their approximate age
+ * @author Darian Pajic
  */
 public class Main {
 
@@ -9,7 +10,27 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+  Scanner input = new Scanner(System.in);  
+    // declare a variable that will store a name
+    String name; 
+
+    // ask the user for their name
+    System.out.println("Please enter your name:");
+    // initialize the name variable with Bob
+    name = input.nextLine();
+    // Say Hello to the user
+    System.out.println("Hello " + name + "!");
+
+    // declare and initialize a constant to store the current year
+    final int CURRENT_YEAR= 2020;
+    // Ask the user for the year they were born 
+    System.out.println("Please enter the year you were born:");
+    // declare and initialize a variable to store the year they were born 
+    int birthYear = input.nextInt();
+
+    // declare and calculate their age
+    int age = CURRENT_YEAR - birthYear; 
+    // Tell the user their age
+    System.out.println("You are " + age + " years old.");
   }
 }
